43d5a14a7fbb74971082a037f7c2f0a5c9fb2031
Shuwen Zhu <shuwenzhu000@gmail.com>
2021-12-03 10:54:07 -0800

    Project Creation

---
+++ src/test/java/com/hrsystem/content/HrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrsystem.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
